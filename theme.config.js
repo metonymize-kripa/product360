82b8f177b80e22fbdaba6e8b15b35d0fe1aa03b4
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-07-14 09:25:39 -0500

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -15,21 +15,21 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Innovation Analytics" />
-      <meta name="og:description" content="Innovation Analytics" />
+      <meta name="description" content="Product 360" />
+      <meta name="og:description" content="Product 360" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:image" content="/japanese-symbol-for-beginner_1f530.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
       <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-      <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+      <meta name="og:image" content="/japanese-symbol-for-beginner_1f530.png" />
+      <meta name="apple-mobile-web-app-title" content="Product360" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/japanese-symbol-for-beginner_1f530.png"/>
+      <link rel="icon" type="image/png" sizes="192x192"  href="/japanese-symbol-for-beginner_1f530.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/japanese-symbol-for-beginner_1f530.png"/>
+      <link rel="icon" type="image/png" sizes="96x96" href="/japanese-symbol-for-beginner_1f530.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/japanese-symbol-for-beginner_1f530.png"/>
+      <meta name="msapplication-TileImage" content="/japanese-symbol-for-beginner_1f530.png"/>
     </>
   ),
   search: true,
@@ -37,5 +37,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()}, © Nextra, Metonymize, Surprisingly Obvious LLC.</>
+  footerText: <>MIT {new Date().getFullYear()}, © Metonymize</>
 }
